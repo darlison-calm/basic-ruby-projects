8816fd5c5627f9d7a8e35631ad22abb7932c2c2d
darlison-calm <bernardo.darlison@gmail.com>
2024-04-25 15:12:43 -0300

    Update sub-strings.rb

--- sub-strings/sub-strings.rb
+++ sub-strings/sub-strings.rb
@@ -12,9 +12,5 @@ end
 
 dictionary = ["below", "down", "go" ,"going", "i", "horn","how","howdy","it","low","own","part","partner","sit"]
 
-# output  { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
-
-
-
-p substrings("below", dictionary)
+substrings("below", dictionary)
 
